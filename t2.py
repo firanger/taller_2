89b47b73ffecf351f97dfec9b14349969b5f6e1c
firanger <jvtjuan15@gmail.com>
2021-09-13 04:35:23 -0500

    primer commit

---
+++ t2.py
@@ -0,0 +1,14 @@
+import random
+"""  
+UNIVERSIDAD DE LA COSTA CUC 
+FUNDAMENTOS ALGORÍTMICOS 
+TALLER DE ALGORITMOS CONDICIONALES 
+PROFESOR: Msc. ROBERTO MORALES  
+
+"""
+"""los ejercicios estan comentados para que sea mas facil ejecutar uno por uno"""
+
+
+
+
+

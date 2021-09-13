a05ac658061b1c5ab49a75c0d67393ffa87b2f05
firanger <jvtjuan15@gmail.com>
2021-09-13 04:37:07 -0500

    ejercicio1

--- t2.py
+++ t2.py
@@ -9,6 +9,18 @@ PROFESOR: Msc. ROBERTO MORALES
 """los ejercicios estan comentados para que sea mas facil ejecutar uno por uno"""
 
 
-
+"""1. Hacer  un  algoritmo  que  calcule  el  total  a  pagar  por  la  compra  de 
+camisas.  Si  se  compran  tres  camisas  o  mas  se  aplica  un  descuento 
+del  30%  sobre  el  total  de  la  compra  y  si  son  menos  de  tres  camisas 
+un descuento del 10%."""
+"""print("el precio de las camisas es de 10000")
+camisa=int(input("ingrese cuantas camisas desea comprar "))
+total=0
+if (camisa>=3):
+    total=(camisa*10000)-((camisa*10000)*0.3)
+    print("el precio total a pagar es de: ", total)
+if(camisa<3):
+    total=(camisa*10000)-((camisa*10000)*0.1)
+    print("el precio total a pagar es de: ", total)"""
 
 
